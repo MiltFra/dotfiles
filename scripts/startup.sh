17b88852e2c92ec6394a76b095894cc7fa324dac
Franz Miltz <franz@miltz.me>
2019-01-21 23:36:20 +0100

    Changed workspaces to fontawesome

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 setxkbmap -layout us,de
 setxkbmap -option 'grp:win_space_toggle'
-i3-msg "workspace 2:shell; append_layout ~/dotfiles/scripts/ws2.json" 
+i3-msg "workspace 2:; append_layout ~/dotfiles/scripts/ws2.json" 
 termite --name htop -e htop &
 termite --name ranger -e ranger & 
 /home/miltfra/.config/i3/bg.sh &
