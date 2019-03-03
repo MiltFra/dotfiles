5cede67c2149de699eabb2d90cc32aee80103d70
Franz Miltz <franz@miltz.me>
2019-03-03 09:26:47 +0100

    Setup 2019-03-03

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 setxkbmap -layout us,de
 setxkbmap -option 'grp:win_space_toggle'
+insync start &> /dev/null &
+insync hide &> /dev/null &
 dropbox &> /dev/null &
 i3-msg "workspace 2:; append_layout ~/dotfiles/scripts/ws2.json" 
 termite --name htop -e htop &
