8a05040593cbf5de145177715d4ed56eba86dece
Franz Miltz <franz@miltz.me>
2019-05-10 15:53:28 +0200

    Switching to BSPWM soon

--- scripts/polybarping.sh
+++ scripts/polybarping.sh
@@ -3,7 +3,7 @@
 HOST=google.com
 
 if ! ping=$(ping -n -c 1 -W 1 $HOST); then
-    echo "# ping failed"
+    echo "--"
 else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
